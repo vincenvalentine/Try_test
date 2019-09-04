3fd24b49edb37f097f358793df148f47e5e13ede
genz18keyz <genz18keyz@gmail.com>
2019-09-04 13:35:30 +0700

    fitur timer & ads bawah

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-$(document).ready(function(){
-    $('#btn-notif').click(function(){
-      $('#notif').hide();
-    });
+// $(document).ready(function(){
+//     $('#btn-notif').click(function(){
+//       $('#notif').hide();
+//     });
+// });
+
+
+// $(document).ready(function(){
+//   $('.close').click(function(){
+//     $('#ads').hide();
+//   });
+// });
+
+
+$( '#btn-notif' ).click(function() {
+    $( '#notif' ).slideUp(500);
+  
+});
+
+// btn-ads
+
+$( '#btn-close' ).click(function() {
+  $( '#ads' ).slideToggle(500);
+});
+
+$("#btn-close").on("click", function(){
+  setTimeout(function(){
+    $('#ads').show();
+  }, 60000)
 });
